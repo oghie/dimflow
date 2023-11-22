f52708bf132383362d8690362a768f447d1a607e
kalimpau <info@kalimpau.com>
2023-11-22 11:03:03 +0000

    fix

--- output/test.py
+++ output/test.py
@@ -1,44 +1,59 @@
 import pandas as pd
 import joblib
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
 def load_model(model_path):
-    """Load the pre-trained Random Forest model."""
     return joblib.load(model_path)
 
 def load_and_prepare_data(csv_path, columns_to_use):
-    """Load CSV data and filter for the specified columns."""
     data = pd.read_csv(csv_path)
-    return data[columns_to_use]
+    filtered_data = data[columns_to_use]
+    return filtered_data
 
 def classify_data(model, data):
-    data_values = data.values  # Convert to numpy array to match training data format
-    predictions = model.predict(data_values)
+    predictions = model.predict(data)
     return predictions
 
 def main():
-    model_path = 'rfnew.pkl'
+    model_path = '23_rf.pkl'
     csv_path = 'ens18.csv'
 
-    # Columns features for prediction
+    # Columns to be used for prediction / 23 features
+
     columns_to_use = [
-        'pkt_len_var', 'fwd_iat_mean', 'tot_bwd_pkts', 'protocol', 'rst_flag_cnt',
-        'psh_flag_cnt', 'active_max', 'fwd_pkt_len_mean', 'totlen_fwd_pkts',
+        'pkt_len_var', 'fwd_iat_mean', 'tot_bwd_pkts', 'protocol', 'psh_flag_cnt',
+        'bwd_iat_max', 'active_max', 'fwd_pkt_len_mean', 'totlen_fwd_pkts',
         'fwd_header_len', 'bwd_iat_min', 'bwd_pkts_s', 'init_bwd_win_byts',
-        'fin_flag_cnt', 'idle_std', 'urg_flag_cnt', 'syn_flag_cnt', 'active_std',
+        'urg_flag_cnt', 'fin_flag_cnt', 'idle_std', 'syn_flag_cnt', 'active_std',
         'fwd_pkts_s', 'fwd_act_data_pkts', 'bwd_iat_tot', 'init_fwd_win_byts',
         'down_up_ratio'
     ]
 
+
+    # Load model
     model = load_model(model_path)
-    data = load_and_prepare_data(csv_path, columns_to_use)
 
-    predictions = classify_data(model, data)
+    data_for_prediction = load_and_prepare_data(csv_path, columns_to_use)
 
-    data['Classification'] = classify_data(model, data)
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(data_for_prediction)
 
-    data.to_csv('predict_data.csv', index=False)
-    print("Classification complete. Results saved to 'predict_data.csv'.")
+    predictions = classify_data(model, scaled_data)
+
+    data = pd.read_csv(csv_path)
+    predictions = ['Malicious' if pred == 1 else 'Benign' for pred in predictions]
+
+    data['Predictions'] = predictions
+    data.to_csv('updated_predictions.csv', index=False)
+'''
+    # Displaying the predictions
+    for i, pred in enumerate(predictions):
+        print(f"Row {i}: Classification - {'Malicious' if pred == 1 else 'Benign'}")
+        data = pd.read_csv(csv_path)
+        data['Predictions'] = predictions
+        data.to_csv('updated_predictions.csv', index=False)
+'''
 
 if __name__ == "__main__":
     main()
